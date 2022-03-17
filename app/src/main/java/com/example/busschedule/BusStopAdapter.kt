cdf075b575d3417d93dff71f4f219fa3b8ffd81a
Christopher Nicholson <nicholsonc@gmail.com>
2022-03-16 21:34:00 -0400

    listadapter, viewholder, and diff callback

---
+++ app/src/main/java/com/example/busschedule/BusStopAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.busschedule
+
+import android.annotation.SuppressLint
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.databinding.BusStopItemBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BusStopAdapter.BusStopViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BusStopAdapter.BusStopViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Schedule>() {
+            override fun areItemsTheSame(oldItem: Schedule, newItem: Schedule): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Schedule, newItem: Schedule): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    class BusStopViewHolder(private var binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SimpleDateFormat")
+        fun bind(schedule: Schedule) {
+            binding.stopNameTextView.text = schedule.stopName
+            binding.arrivalTimeTextView.text = SimpleDateFormat(
+                "h:mm a").format(
+                Date(schedule.arrivalTime.toLong() * 1000)
+            )
+        }
+    }
+}
