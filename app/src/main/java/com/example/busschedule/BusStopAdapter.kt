d81aa1fac299bf1c19062e0bec04c444e941f084
Christopher Nicholson <nicholsonc@gmail.com>
2022-03-16 22:22:31 -0400

    app runs with global coroutine

--- app/src/main/java/com/example/busschedule/BusStopAdapter.kt
+++ app/src/main/java/com/example/busschedule/BusStopAdapter.kt
@@ -1,26 +1,19 @@
 package com.example.busschedule
 
 import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.databinding.BusStopItemBinding
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
 
-class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback) {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): BusStopAdapter.BusStopViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: BusStopAdapter.BusStopViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+class BusStopAdapter(
+    private val onItemClicked: (Schedule) -> Unit
+) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback) {
 
     companion object {
         private val DiffCallback = object : DiffUtil.ItemCallback<Schedule>() {
@@ -34,13 +27,33 @@ class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapte
         }
     }
 
-    class BusStopViewHolder(private var binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BusStopViewHolder {
+        val viewHolder = BusStopViewHolder(
+            BusStopItemBinding.inflate(
+                LayoutInflater.from( parent.context),
+                parent,
+                false
+            )
+        )
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            onItemClicked(getItem(position))
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: BusStopViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class BusStopViewHolder(
+        private var binding: BusStopItemBinding
+    ): RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SimpleDateFormat")
         fun bind(schedule: Schedule) {
             binding.stopNameTextView.text = schedule.stopName
             binding.arrivalTimeTextView.text = SimpleDateFormat(
-                "h:mm a").format(
-                Date(schedule.arrivalTime.toLong() * 1000)
+                "h:mm a").format(Date(schedule.arrivalTime.toLong() * 1000)
             )
         }
     }
--- app/src/main/java/com/example/busschedule/database/AppDatabase.kt
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -7,10 +7,9 @@ import androidx.room.RoomDatabase
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
 
-@Database(entities = arrayOf(Schedule::class), version = 1)
 
+@Database(entities = arrayOf(Schedule::class), version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
-
     abstract fun scheduleDao(): ScheduleDao
 
     companion object {
