21e64f88fe1249d3bb87f39353f62d6f63642c33
Christopher Nicholson <nicholsonc@gmail.com>
2022-04-24 15:26:07 -0400

    starting but might not finish

---
+++ app/src/main/java/com/example/busschedule/MapsFragment.kt
@@ -0,0 +1,16 @@
+package com.example.busschedule
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+
+class MapsFragment : AppCompatActivity(), OnMapReadyCallback {
+
+
+    override fun onMapReady(p0: GoogleMap) {
+
+
+
+        TODO("Not yet implemented")
+    }
+}
