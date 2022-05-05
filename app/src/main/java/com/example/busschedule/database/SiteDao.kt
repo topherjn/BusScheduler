6d05da4a913525082ae8e13fb07371702c0b0c2e
Christopher Nicholson <nicholsonc@gmail.com>
2022-05-05 19:33:10 -0400

    Dao updated to do more CRUD

--- app/src/main/java/com/example/busschedule/database/SiteDao.kt
+++ app/src/main/java/com/example/busschedule/database/SiteDao.kt
@@ -1,11 +1,19 @@
 package com.example.busschedule.database
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiteDao {
     @Query("SELECT * FROM site WHERE arrondissement = :arrondissement ORDER BY site_name")
     fun getSitesByArrondissement(arrondissement: Int): Flow<List<Site>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertSite(site: Site)
+
+    @Update
+    fun updateSite(site: Site)
+
+    @Delete
+    fun delete(site: Site)
 }
